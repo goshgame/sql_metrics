a1c12e6dbaae16828c3df011a6a49a7a8969a5a1
BensonZhou <BensonZhou@github.com>
2025-09-16 21:03:58 +0800

    feature: sql metrics

--- conn.go
+++ conn.go
@@ -1,6 +1,7 @@
 package sqlmetrics
 
 import (
+	"context"
 	"database/sql/driver"
 	"time"
 )
@@ -31,4 +32,58 @@ func (mc *metricsConn) Begin() (driver.Tx, error) {
 	tx, err := mc.Conn.Begin()
 	reportMetrics("", "begin", startTime, err)
 	return tx, err
+}
+
+// ExecContext
+func (mc *metricsConn) ExecContext(ctx context.Context, query string, args []driver.NamedValue) (driver.Result, error) {
+	execer, ok := mc.Conn.(driver.ExecerContext)
+	if !ok {
+		// fallback 到老接口
+		return nil, driver.ErrSkip
+	}
+	start := time.Now()
+	res, err := execer.ExecContext(ctx, query, args)
+	reportMetrics(query, "exec", start, err)
+	return res, err
+}
+
+// QueryContext
+func (mc *metricsConn) QueryContext(ctx context.Context, query string, args []driver.NamedValue) (driver.Rows, error) {
+	queryer, ok := mc.Conn.(driver.QueryerContext)
+	if !ok {
+		// fallback
+		return nil, driver.ErrSkip
+	}
+	start := time.Now()
+	rows, err := queryer.QueryContext(ctx, query, args)
+	reportMetrics(query, "query", start, err)
+	return rows, err
+}
+
+// PrepareContext
+func (mc *metricsConn) PrepareContext(ctx context.Context, query string) (driver.Stmt, error) {
+	preparer, ok := mc.Conn.(driver.ConnPrepareContext)
+	if !ok {
+		// fallback
+		return mc.Prepare(query)
+	}
+	start := time.Now()
+	stmt, err := preparer.PrepareContext(ctx, query)
+	reportMetrics(query, "prepare", start, err)
+	if err != nil {
+		return nil, err
+	}
+	return &metricsStmt{Stmt: stmt, query: query}, nil
+}
+
+// Ping
+func (mc *metricsConn) Ping(ctx context.Context) error {
+	pinger, ok := mc.Conn.(driver.Pinger)
+	if !ok {
+		return driver.ErrSkip
+	}
+	start := time.Now()
+	err := pinger.Ping(ctx)
+	reportMetrics("", "ping", start, err)
+	return err
 }
